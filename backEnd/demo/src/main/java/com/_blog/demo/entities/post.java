1592c8ff434542ed2ccb03d2cca8d1912eca4ec5
RedaAz07 <annizreda07@gmail.com>
2026-04-01 18:57:50 +0100

    d

--- backEnd/demo/src/main/java/com/_blog/demo/entities/post.java
+++ backEnd/demo/src/main/java/com/_blog/demo/entities/post.java
@@ -1,5 +1,17 @@
 package com._blog.demo.entities;
 
+import jakarta.persistence.GeneratedValue;
+
 public class post {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
 }
--- backEnd/demo/src/main/java/com/_blog/demo/entities/user.java
+++ backEnd/demo/src/main/java/com/_blog/demo/entities/user.java
@@ -2,25 +2,33 @@ package com._blog.demo.entities;
 
 import java.util.Date;
 
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+
 @Entity
 @jakarta.persistence.Table(name = "users")
 @Getter
 @Setter
-
 public class user {
-
+    @Column(name = "uuid", nullable = false, unique = true)
+    @UuidGenerator
     private String uuid;
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @Column(name = "password", nullable = false)
     private String password;
+    @Column(name = "role", nullable = false)
     private String role;
     private String firstName;
     private String lastName;
     private Date birthDate;
     private String profilePictureUrl;
-    private boolean  status;
+    private boolean status;
 
 }
