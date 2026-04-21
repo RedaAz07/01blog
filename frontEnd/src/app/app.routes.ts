37afc67fc70ba431167a607f76d494af4db5868f
RedaAz07 <annizreda07@gmail.com>
2026-04-21 14:57:27 +0100

    start angulare

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login-component/login-component';
+import { NotFoundComponent } from './not-found-component/not-found-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: '**', redirectTo: 'not-found'}
+];
