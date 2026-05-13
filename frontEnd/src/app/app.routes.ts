5a3ac5b7f3c655d100e03142353b64b2c772c6b8
RedaAz07 <annizreda07@gmail.com>
2026-05-13 12:54:49 +0100

    fix routin

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -11,35 +11,19 @@ import { guestGuard } from './core/guards/guest-guard';
 import { roleGuard } from './core/guards/role-guard';
 import { AdminDashboardComponent } from './admin-dashboard-component/admin-dashboard-component';
 export const routes: Routes = [
-  { path: 'login', component: Login, canActivate: [guestGuard] },
+ { path: 'login', component: Login, canActivate: [guestGuard] },
   { path: 'register', component: Register, canActivate: [guestGuard] },
   {
     path: '',
     component: MainLayout,
     canActivate: [authGuard],
     children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: Home },
       { path: 'profile/:username', component: Profile },
       { path: 'dashboard', canActivate: [roleGuard], component: AdminDashboardComponent },
     ],
   },
-
-  /*  {
-      path: 'admin',
-      canActivate: [authGuard, roleGuard],
-      data: { role: 'ADMIN' },
-
-      children: [
-        { path: 'ban-user', component: BanUserComponent },
-        { path: 'hide-post', component: HidePostComponent },
-        { path: 'delete-user', component: DeleteUserComponent },
-        { path: 'delete-post', component: DeletePostComponent },
-
-        // Optional: A default dashboard if they just type /admin
-        { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-        { path: 'dashboard', component: AdminDashboardComponent },
-      ], 
-    } */
   { path: 'not-found', component: NotFound },
   { path: '**', redirectTo: 'not-found' },
 ];
